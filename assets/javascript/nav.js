315ce737d4ca8df88bae5da5357620f0f003690a
henroca <jeff.hen@hotmail.com>
2015-11-12 13:26:52 -0200

    nav width window

--- assets/javascript/nav.js
+++ assets/javascript/nav.js
@@ -7,64 +7,66 @@ for(var i = 0;i < 5;i++){
 var isTrue = true;
 var i = 0;
 $(function(){
-  var bannerHeight = $('#banner').height();
-  animateFront = function(){
-    if($(window).scrollTop() < bannerHeight)animateBack();
-    i++;
-    if(i < 5){
-      logo.attr("src",imagesArray[i]);
-      setTimeout("animateFront()", 130);
-    }
-  }
-  animateBack = function(){
-    if($(window).scrollTop() > bannerHeight)animateFront();
-    i--;
-    if(i >= 0){
-      logo.attr("src",imagesArray[i]);
-      setTimeout("animateBack()", 130);
-    }
-  }
-  var nav = $('#box-nav');
-	var aside = $('#games-aside');
-  var logo = $("#logo");
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > bannerHeight && isTrue){
-      isTrue = false;
-			nav.addClass("nav-fixed");
-      logo.removeClass("logo-absolute");
-      animateFront();
-		}else if($(this).scrollTop() < bannerHeight && !isTrue){
-      isTrue = true
-      logo.addClass("logo-absolute");
-			nav.removeClass("nav-fixed");
-      animateBack();
-		}
-    if ($(this).scrollTop() > bannerHeight){
-      aside.addClass("aside-fixed");
-      $('#games-aside a').removeClass('active');
-      $('#games-aside a[href = #ESPN]').addClass('active')
-  		if ($(this).scrollTop() > 1100){
-  			$('#games-aside a').removeClass('active');
-  			$('#games-aside a[href = #antidoping]').addClass('active');
-
-        if ($(this).scrollTop() > 2300){
-          $('#games-aside a').removeClass('active');
-          $('#games-aside a[href = #dota-2]').addClass('active');
-
-          if ($(this).scrollTop() > 3000){
-          	$('#games-aside a').removeClass('active');
-          	$('#games-aside a[href = #league-of-legends]').addClass('active');
-
-            if ($(this).scrollTop() > 4000){
-              $('#games-aside a').removeClass('active');
-              $('#games-aside a[href = #counter-strike]').addClass('active');
-            }
-        	}
+    var bannerHeight = $('#banner').height();
+    if($(window).width() > 300){
+        animateFront = function(){
+          if($(window).scrollTop() < bannerHeight)animateBack();
+          i++;
+          if(i < 5){
+            logo.attr("src",imagesArray[i]);
+            setTimeout("animateFront()", 130);
+          }
         }
-  		}
-    }else {
-        $('#games-aside a').removeClass('active');
-      	aside.removeClass("aside-fixed");
-    }
-	});
+        animateBack = function(){
+          if($(window).scrollTop() > bannerHeight)animateFront();
+          i--;
+          if(i >= 0){
+            logo.attr("src",imagesArray[i]);
+            setTimeout("animateBack()", 130);
+          }
+        }
+        var nav = $('#box-nav');
+      	var aside = $('#games-aside');
+        var logo = $("#logo");
+      	$(window).scroll(function () {
+      		if ($(this).scrollTop() > bannerHeight && isTrue){
+            isTrue = false;
+      			nav.addClass("nav-fixed");
+            logo.removeClass("logo-absolute");
+            animateFront();
+      		}else if($(this).scrollTop() < bannerHeight && !isTrue){
+            isTrue = true
+            logo.addClass("logo-absolute");
+      			nav.removeClass("nav-fixed");
+            animateBack();
+      		}
+          if ($(this).scrollTop() > bannerHeight){
+            aside.addClass("aside-fixed");
+            $('#games-aside a').removeClass('active');
+            $('#games-aside a[href = #ESPN]').addClass('active')
+        		if ($(this).scrollTop() > 1100){
+        			$('#games-aside a').removeClass('active');
+        			$('#games-aside a[href = #antidoping]').addClass('active');
+
+              if ($(this).scrollTop() > 2300){
+                $('#games-aside a').removeClass('active');
+                $('#games-aside a[href = #dota-2]').addClass('active');
+
+                if ($(this).scrollTop() > 3000){
+                	$('#games-aside a').removeClass('active');
+                	$('#games-aside a[href = #league-of-legends]').addClass('active');
+
+                  if ($(this).scrollTop() > 4000){
+                    $('#games-aside a').removeClass('active');
+                    $('#games-aside a[href = #counter-strike]').addClass('active');
+                  }
+              	}
+              }
+        		}
+          }else {
+              $('#games-aside a').removeClass('active');
+            	aside.removeClass("aside-fixed");
+          }
+      	});
+      }
 });
